cb9a573db63c53f3a091105f34a0ac5cbb2b0258
caglaror <caglaror@gmail.com>
2019-03-23 00:16:00 -0700

    Range type input value sync with css parameters.

--- js/index.js
+++ js/index.js
@@ -1,17 +1,33 @@
-window.addEventListener('load',function(){totalLoad()});
+window.addEventListener('load',function(){totalLoad();});
 
 
 function totalLoad(){
     const controls = document.querySelectorAll('.controls input');
-
-    controls.forEach(input => input.addEventListener('change',function(){
-
-        console.log('sysnc done:'+ input.value);
-        let sizing = input.dataset.sizing || '';
-            document.documentElement.style.setProperty(`--${this.name}`,`${input.value}${sizing}`);
+    controls.forEach(input => input.addEventListener('mousedown',sync,true));
+    controls.forEach(input => input.addEventListener('mouseup',unsync,true));
+}
 
 
-    }));
+function sync(event){
+    let input = event.target;
+    console.log(`Syncing... Last value is ${input.value}`);
+    input.addEventListener('mousemove',detector,true) ;
+}
 
+
+function detector(event){
+    let input = event.target;
+    console.log('sysnc done:'+ input.value);
+    let sizing = input.dataset.sizing || '';
+    document.documentElement.style.setProperty(`--${this.name}`,`${input.value}${sizing}`);
+
+}
+
+
+
+function unsync(event){
+    let input = event.target;
+    console.log(`Unsyncing... Last value is ${input.value}`);
+        input.removeEventListener('mousemove',detector,true);
 
 }
